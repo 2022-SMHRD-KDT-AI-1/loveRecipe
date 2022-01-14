b90d448a264a54f656f27214944ab0834b34cb0e
smhrd <smhrd@220.80.165.69>
2022-01-14 11:43:12 +0900

    aaaa

--- gittest/src/gittest/test1.java
+++ gittest/src/gittest/test1.java
@@ -2,7 +2,8 @@ package gittest;
 
 public class test1 {
 	public static void main(String[] args) {
-		System.out.println("첫번째 커밋");
+		System.out.println("첫번째 커밋!");
+		System.out.println("두번째 커밋!><");
 	
 	}
 	
