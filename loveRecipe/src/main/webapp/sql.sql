6b60d568ff02efb2bd5852f464b74423eb7fed6a
mysteeDream <quebec706@gmail.com>
2022-01-28 16:08:18 +0900

    레시피 테이블 테스트

--- loveRecipe/src/main/webapp/sql.sql
+++ loveRecipe/src/main/webapp/sql.sql
@@ -134,7 +134,7 @@ values(recipe_seq.nextval,'약과', '떡,한과', '한식', '135',
 select * from recipe;
 
 -- 전체 행 갯수 가져오기
-select count * from recipe;
+select count (*) from recipe;
 
 -- 데이터 삭제 (레시피 이름으로 지정)
 delete from recipe where recipe_name in('abc');
