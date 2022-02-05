24e015ac822a953e4b40f45daf0b9fa0247cf862
smhrd <smhrd@220.80.165.69>
2022-02-05 11:14:10 +0900

    받는중

--- loveRecipe/src/main/java/model/menuDTO.java
+++ loveRecipe/src/main/java/model/menuDTO.java
@@ -1,7 +1,52 @@
 package model;
 
 public class menuDTO {
-	private String main;
-	private String message;
 
+	private String ingrident;
+	private String object;
+	private String type;
+	private String seasonal;
+	private String kcal;
+	public menuDTO(String ingrident, String object, String type, String seasonal, String kcal) {
+		super();
+		this.ingrident = ingrident;
+		this.object = object;
+		this.type = type;
+		this.seasonal = seasonal;
+		this.kcal = kcal;
+	}
+	public String getIngrident() {
+		return ingrident;
+	}
+	public void setIngrident(String ingrident) {
+		this.ingrident = ingrident;
+	}
+	public String getObject() {
+		return object;
+	}
+	public void setObject(String object) {
+		this.object = object;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSeasonal() {
+		return seasonal;
+	}
+	public void setSeasonal(String seasonal) {
+		this.seasonal = seasonal;
+	}
+	public String getKcal() {
+		return kcal;
+	}
+	public void setKcal(String kcal) {
+		this.kcal = kcal;
+	}
+
+
+
+	
 }
