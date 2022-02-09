eff2c71be3270324c025012e9b22507313d344e7
mysteeDream <quebec706@gmail.com>
2022-02-09 17:30:50 +0900

    마지막 push

--- loveRecipe/src/main/java/model/refivo.java
+++ loveRecipe/src/main/java/model/refivo.java
@@ -4,7 +4,6 @@ public class refivo {
 	private int ingre_amount;
 	private String ingre_temp;
 	private String ingre_name;
-<<<<<<< HEAD
 
 	public refivo(int ingre_amount, String ingre_temp, String ingre_name) {
 		this.ingre_amount = ingre_amount;
@@ -37,32 +36,3 @@ public class refivo {
 	}
 
 }
-=======
-	public refivo(int ingre_amount, String ingre_temp, String ingre_name) {
-		this.ingre_amount = ingre_amount;
-		this.ingre_temp = ingre_temp;
-		this.ingre_name = ingre_name;
-	}
-	public int getIngre_amount() {
-		return ingre_amount;
-	}
-	public void setIngre_amount(int ingre_amount) {
-		this.ingre_amount = ingre_amount;
-	}
-	public String getIngre_temp() {
-		return ingre_temp;
-	}
-	public void setIngre_temp(String ingre_temp) {
-		this.ingre_temp = ingre_temp;
-	}
-	public String getIngre_name() {
-		return ingre_name;
-	}
-	public void setIngre_name(String ingre_name) {
-		this.ingre_name = ingre_name;
-	}
-	
-	
-	
-}
->>>>>>> refs/remotes/origin/master
